2b37885d0cfec72c0e6d561b331f1d6df98a0f61
Wyzincsmarthome <geral@wyzinc.pt>
2025-06-04 10:45:54 +0100

    Update categorization.py

--- categorization.py
+++ categorization.py
@@ -1,19 +1,43 @@
-def map_main_category(product_family):
-    casa_inteligente = [
-        "Assistentes Virtuais", "Campainha Inteligente", "Controlo Remoto",
-        "Fechaduras Inteligentes", "Hubs Inteligentes", "Iluminação",
-        "Interruptor Inteligente", "Motor Cortinas", "Painel Controlo",
-        "Termostato Inteligente", "Tomadas", "Gadgets Inteligentes"
-    ]
-    gadgets = [
-        "Aspiradores", "Aspirador Vertical", "Aspirador Robô", "Mini Aspirador",
-        "Acessórios Aspiradores", "Auscultadores", "Cozinha", "Smartwatch",
-        "Gadgets P/ Animais", "Gadgets Diversos"
-    ]
+def get_tags_for_product(product):
+    tags = []
 
-    if product_family in casa_inteligente:
-        return "Casa Inteligente"
-    elif product_family in gadgets:
-        return "Gadgets"
-    else:
-        return "Sem categoria principal"
+    brand = product.get("brand", "").strip()
+    if brand:
+        tags.append(brand)
+
+    family = product.get("family", "").strip()
+    if family:
+        tags.append(family)
+
+    sub_family = product.get("sub_family", "")
+    if sub_family:
+        tags.append(sub_family.strip())
+
+    family_to_main = {
+        "Assistentes Virtuais": "Casa Inteligente",
+        "Campainha Inteligente": "Casa Inteligente",
+        "Fechaduras Inteligentes": "Casa Inteligente",
+        "Hubs Inteligentes": "Casa Inteligente",
+        "Iluminação": "Casa Inteligente",
+        "Interruptor Inteligente": "Casa Inteligente",
+        "Motor Cortinas": "Casa Inteligente",
+        "Painel Controlo": "Casa Inteligente",
+        "Termostato Inteligente": "Casa Inteligente",
+        "Tomadas": "Casa Inteligente",
+        "Gadgets Inteligentes": "Casa Inteligente",
+        "Auscultadores": "Gadgets",
+        "Smartwatch": "Gadgets",
+        "Cozinha": "Gadgets",
+        "Gadgets P/ Animais": "Gadgets",
+        "Gadgets Diversos": "Gadgets",
+        "Aspiradores": "Gadgets",
+        "Mini Aspirador": "Gadgets",
+        "Aspirador Vertical": "Gadgets",
+        "Aspirador Robô": "Gadgets",
+    }
+
+    main_cat = family_to_main.get(family, None)
+    if main_cat:
+        tags.append(main_cat)
+
+    return tags
